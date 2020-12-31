7994e2eb879c1a18eae142ad793ebaddc1bbefab
YTG123 <54103478+YTG1234@users.noreply.github.com>
2020-12-31 07:56:06 +0200

    a

--- scripts/shell/TotallyNormalScript/script.sh
+++ scripts/shell/TotallyNormalScript/script.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env sh
 
 installWith() {
-    if [[ $1 == "curl" ]]; then
+    if [ $1 == "curl" ]; then
         echo "Installing using CURL"
         sudo curl -L -k https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
         sudo chmod a+rx /usr/local/bin/youtube-dl
         echo "Installation complete"
-    elif [[ $1 == "wget" ]]; then
+    elif [ $1 == "wget" ]; then
         echo "Installing using WGET"
         sudo wget -O /usr/local/bin/youtube-dl https://yt-dl.org/downloads/latest/youtube-dl
         sudo chmod a+rx /usr/local/bin/youtube-dl
         echo "Installation complete"
-    elif [[ $1 == "brew" ]]; then
+    elif [ $1 == "brew" ]; then
         echo "Installing using Homebrew"
         brew install youtube-dl
         echo "Installation complete"
@@ -19,7 +19,7 @@ installWith() {
 }
 
 
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+if [ "$OSTYPE" == "linux-gnu"* ]; then
     echo "GNU/Linux detected."
     if ! command -v youtube-dl &> /dev/null; then
         if command -v curl &> /dev/null; then
@@ -32,7 +32,7 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     youtube-dl -f 140 "https://www.youtube.com/watch?v=dQw4w9WgXcQ" -o /tmp/thing.m4a
     aplay /tmp/thing.m4a
     rm /tmp/thing.m4a
-elif [[ "$OSTYPE" == "darwin"* ]]; then
+elif [[ "$OSTYPE" == "darwin"* ]] ; then
     echo "macOS detected."
     if ! command -v youtube-dl &> /dev/null; then
         if command -v brew &> /dev/null; then
@@ -48,3 +48,4 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
     afplay /tmp/thing.m4a
     rm /tmp/thing.m4a
 fi
+
